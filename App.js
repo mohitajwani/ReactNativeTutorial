14d25383f51c6a88e0ae69219fbb1dd88be1d997
Mohit Ajwani <mohitajwani@gmail.com>
2019-03-29 23:02:51 +0530

    Initial

---
+++ App.js
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, TextInput, View } from 'react-native';
+import LoginView from './login/LoginView';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu,\n' +
+    'Welcome to Awesome Project',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+  /*render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }*/
+  render() {
+    return (
+      /* <View style={{ padding: 10 }}>
+        <TextInput
+          style={{ height: 60, fontSize: 20 }}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({ text })}
+        />
+        <Text style={{ padding: 10, fontSize: 42 }}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+      </View> */
+      <View><LoginView /></View>
+    );
+  }
+}
---
+++ search/SearchView.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Image, Button, Text, TextInput, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview'
+
+class SearchView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <KeyboardAwareScrollView>
+                <View style={{
+                    flex: 1,
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    alignSelf: 'center',
+                    display: 'flex',
+                    margin: 10
+                }}>
+                    <Image source={{ uri: 'https://reactjs.org/logo-og.png' }}
+                        style={{ width: 80, height: 80, marginTop: 5 }}
+                    />
+                    <TextInput
+                        style={{ height: 60, marginTop: 5, fontSize: 20, backgroundColor: '#C5C5C5' }}
+                        placeholder="Email"
+                        onChangeText={(text) => this.setState({ text })}
+                    />
+                    <TextInput
+                        style={{ height: 60, marginTop: 5, fontSize: 20, backgroundColor: '#C5C5C5' }}
+                        placeholder="Password"
+                        onChangeText={(text) => this.setState({ text })}
+                    />
+                    <Button
+                        // onPress={onPressSignIn}
+                        style={{
+                            width: 260,
+                            alignItems: 'center',
+                            padding: 10,
+                        }}
+                        title="Sign In"
+                        color="#841584"
+                        accessibilityLabel="Sign In using this button">
+                    </Button>
+                </View>
+            </KeyboardAwareScrollView>
+        );
+    }
+}
+
+export default SearchView;
